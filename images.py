3b2d254e4724fa361163f95074b608058a64ec11
Rick <r.devries-13@student.utwente.nl>
2026-01-29 20:31:21 +0100

    Jacky image

--- images.py
+++ images.py
@@ -5,3 +5,6 @@ piper_image = pygame.transform.scale(piper_image, (50, 50))
 
 spike_image = pygame.image.load("images/spike.png")
 spike_image = pygame.transform.scale(spike_image, (50, 50))
+ 
+jacky_image = pygame.image.load("images/jacky.png")
+jacky_image = pygame.transform.scale(jacky_image, (50, 50))
